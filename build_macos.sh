fbc03da0c0720926dbc246fa617863ad07370561
Philippe Gagne <philippe.gagne@optelgroup.com>
2020-09-15 10:00:37 -0400

    More info.

--- build_macos.sh
+++ build_macos.sh
@@ -8,6 +8,12 @@ cmake -D CMAKE_BUILD_TYPE=Release \
    -D HAVE_opencv_python3=ON \
    -DOPENCV_GENERATE_PKGCONFIG=ON \
    ../opencv
-make
+make -J2
 make install
 
+# -DWITH_MATLAB=0
+#-DPYTHON3_EXECUTABLE=${SRC_DIR}/py3/bin/python                          \
+#    -DPYTHON3_INCLUDE_DIR=${SRC_DIR}/py3/include/python3.7m                 \
+#    -DPYTHON3_NUMPY_INCLUDE_DIRS=${SRC_DIR}/py3/lib/python3.7/site-packages/numpy/core/include   \
+#    -DPYTHON3_LIBRARY=${SRC_DIR}/py3/lib/libpython3.7m.${DYNAMIC_EXT}       \
+#    -DPYTHON3_PACKAGES_PATH=${SRC_DIR}/py3/lib/python3.7/site-packages   
