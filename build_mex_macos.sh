2f048777543e6573968d0562c1e481c0e32d3695
Philippe Gagné <philippemgagne@me.com>
2020-09-18 12:23:52 -0400

    Build mexopencv with opencv 3.4.1.

---
+++ build_mex_macos.sh
@@ -0,0 +1,31 @@
+export PKG_CONFIG_PATH=$(pwd)/dist/lib/pkgconfig
+cd mexopencv
+#make clean MATLABDIR=/Applications/MATLAB_R2019b.app PKG_CONFIG_OPENCV=opencv WITH_CONTRIB=1  -j2
+#make all MATLABDIR=/Applications/MATLAB_R2019b.app PKG_CONFIG_OPENCV=opencv WITH_CONTRIB=1 CXXFLAGS="CFLAGS='$CFLAGS -Wno-deprecated-declarations -Wno-potentially-evaluated-expression'" 
+#make contrib MATLABDIR=/Applications/MATLAB_R2019b.app PKG_CONFIG_OPENCV=opencv WITH_CONTRIB=1 CXXFLAGS="CFLAGS='$CFLAGS -Wno-deprecated-declarations -Wno-potentially-evaluated-expression'" 
+
+cd ..
+rm -rf dist/mexopencv/*
+cp -r mexopencv/*.m dist/mexopencv
+cp -r mexopencv/+mexopencv dist/mexopencv
+cp -r mexopencv/+cv dist/mexopencv
+cp -r mexopencv/opencv_contrib/+cv/* dist/mexopencv/+cv
+cp -r dist/lib dist/mexopencv
+cp -r mexopencv/test dist/mexopencv
+cp -r mexopencv/samples dist/mexopencv
+cp -r mexopencv/opencv_contrib/samples/* dist/mexopencv/samples
+
+for f in dist/mexopencv/+cv/*.mexmaci64
+do
+   install_name_tool -add_rpath @loader_path/../lib $f
+done
+
+for f in dist/mexopencv/+cv/private/*.mexmaci64
+do
+   install_name_tool -add_rpath @loader_path/../../lib $f
+done
+
+#for f in opencv_contrib/+cv/private/*.mexmaci64 
+#do
+#   install_name_tool -add_rpath @loader_path/../../../lib f
+#done
