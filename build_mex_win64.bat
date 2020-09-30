cc4047bec27ec6b79ad0342b0be4652f864ae571
Philippe Gagne <philippe.gagne@optelgroup.com>
2020-09-30 11:35:42 -0400

    No confirmation for copy.

--- build_mex_win64.bat
+++ build_mex_win64.bat
@@ -8,17 +8,17 @@ cd mexopencv
 git checkout 3.4.11
 cd ..
 
-copy mexopencv_setup.m mexopencv
-copy dist\x64\vc16\bin\*dll mexopencv\lib
+copy /y mexopencv_setup.m mexopencv
+copy /Y dist\x64\vc16\bin\*dll mexopencv\lib
 "C:\Program Files\MATLAB\R2020b\bin\matlab" -nodesktop -wait -sd . -r "mex_compile; quit"
 "C:\Program Files\MATLAB\R2020b\bin\matlab" -nodesktop -wait -sd . -r "cd mexopencv;mexopencv_setup;addpath('utils');MDoc('-clean'); MDoc; quit"
 
 
-mkdir dist\mexopencv\bin
-copy dist\x64\vc16\bin\*.exe dist\mexopencv\bin
-copy mexopencv\*.m  mexopencv\README.markdown mexopencv\info.xml  dist\mexopencv
-copy mexopencv\+cv dist\mexopencv
-copy mexopencv\+mexopencv dist\mexopencv
-copy mexopencv\doc dist\mexopencv
-copy mexopencv\samples dist\mexopencv
-copy mexopencv\test dist\mexopencv
+mkdir /y dist\mexopencv\bin
+copy /y dist\x64\vc16\bin\*.exe dist\mexopencv\bin
+copy /y mexopencv\*.m  mexopencv\README.markdown mexopencv\info.xml  dist\mexopencv
+copy /y mexopencv\+cv dist\mexopencv
+copy /y mexopencv\+mexopencv dist\mexopencv
+copy /y mexopencv\doc dist\mexopencv
+copy /y mexopencv\samples dist\mexopencv
+copy /y mexopencv\test dist\mexopencv
