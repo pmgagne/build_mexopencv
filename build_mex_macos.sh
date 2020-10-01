91ad994c5e2a2012ea9be78bbf0aaae2b0461994
Philippe Gagné <philippemgagne@me.com>
2020-10-01 18:39:01 -0400

    File reorg.

--- build_mex_macos.sh
+++ build_mex_macos.sh
@@ -1,49 +1,51 @@
-if [ -d dist/mexopencv ]
-then
-   rm -rf dist/mexopencv/*
-else
-   mkdir dist/mexopencv
-fi
-
 export PKG_CONFIG_PATH=$(pwd)/dist/lib/pkgconfig
 cd mexopencv
 git checkout 3.4.11
+git pull
+
+cp -R ../dist opencv
+cp ../mexopencv_setup.m .
+cp ../mexopencv_mac64.prj .
 
 make clean MATLABDIR=/Applications/MATLAB_R2019b.app PKG_CONFIG_OPENCV=opencv WITH_CONTRIB=1
 make all MATLABDIR=/Applications/MATLAB_R2019b.app PKG_CONFIG_OPENCV=opencv WITH_CONTRIB=1  CXXFLAGS="CFLAGS='$CFLAGS -Wno-deprecated-declarations -Wno-potentially-evaluated-expression'" 
 make contrib MATLABDIR=/Applications/MATLAB_R2019b.app PKG_CONFIG_OPENCV=opencv WITH_CONTRIB=1 LDFLAGS="-lopencv_img_hash" CXXFLAGS="CFLAGS='$CFLAGS -Wno-deprecated-declarations -Wno-potentially-evaluated-expression'" 
 
-cd ..
-
-cp -r mexopencv/opencv_contrib/+cv/* mexopencv/+cv
-cp -r mexopencv/opencv_contrib/samples/* mexopencv/samples
-cp -r mexopencv/opencv_contrib/test/* mexopencv/test
-cp  dist/lib/*.dylib mexopencv/lib
-
-for f in mexopencv/+cv/*.mexmaci64
+for f in +cv/*.mexmaci64
 do
-   install_name_tool -delete_rpath @loader_path/../../lib $f
-   install_name_tool -delete_rpath @loader_path/../../../lib $f
-   install_name_tool -add_rpath @loader_path/../lib $f
+   install_name_tool -add_rpath @loader_path/../opencv/lib $f
 done
 
-for f in mexopencv/+cv/private/*.mexmaci64
+for f in +cv/private/*.mexmaci64
 do
-   install_name_tool -delete_rpath @loader_path/../../../lib $f
-   install_name_tool -add_rpath @loader_path/../../lib $f
+   install_name_tool -add_rpath @loader_path/../../opencv/lib $f
 done
 
-/Applications/MATLAB_R2019b.app/bin/matlab -nodesktop -wait -sd . -r "cd mexopencv;addpath(pwd);addpath(fullfile(pwd, 'utils'));MDoc('-clean');MDoc('-wiki');MDoc; quit"
+for f in opencv_contrib/+cv/*.mexmaci64
+do
+   install_name_tool -add_rpath @loader_path/../../opencv/lib $f
+done
 
-rm -rdf dist/mexopencv/*
-mkdir dist/mexopencv/lib
+for f in opencv_contrib/+cv/private/*.mexmaci64
+do
+   install_name_tool -add_rpath @loader_path/../../../opencv/lib $f
+done
 
-cp mexopencv/*.m dist/mexopencv
-cp -r mexopencv/+mexopencv dist/mexopencv
-cp -r mexopencv/+cv dist/mexopencv
-cp -r dist/lib/* dist/mexopencv/lib
-cp -r mexopencv/test dist/mexopencv
-cp -r mexopencv/samples dist/mexopencv
-cp mexopencv_setup.m dist/mexopencv
-cp -r mexopencv/doc dist/mexopencv
+/Applications/MATLAB_R2019b.app/bin/matlab -nodesktop -sd . -r "addpath(pwd);addpath(fullfile(pwd, 'utils'));MDoc('-clean');MDoc('-wiki');MDoc; quit"
+
+# rm -rdf dist/mexopencv/*
+# mkdir dist/mexopencv/lib
+# mkdir dist/mexopencv/opencv_contrib
+
+# cp mexopencv/*.m dist/mexopencv
+# cp -r mexopencv/+mexopencv dist/mexopencv
+# cp -r mexopencv/+cv dist/mexopencv
+# cp -r dist/lib/* dist/mexopencv/lib
+# cp -r mexopencv/test dist/mexopencv
+# cp -r mexopencv/samples dist/mexopencv
+# cp -r mexopencv/opencv_contrib/test dist/mexopencv/opencv_contrib
+# cp -r mexopencv/opencv_contrib/samples dist/mexopencv/opencv_contrib
+
+# cp mexopencv_setup.m dist/mexopencv
+# cp -r mexopencv/doc dist/mexopencv
 
